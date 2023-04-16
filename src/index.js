51e77dbb6835b855af465e41a9d4f7a99b4846ba
aniajad <116589679+aniajad@users.noreply.github.com>
2023-04-16 12:35:16 +0200

    Fixed a small bug with cities that have double name

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ function updateCity(event) {
     cityTimeZone = moment.tz.guess();
   }
 
-  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = ` <div class="container" id="city">
